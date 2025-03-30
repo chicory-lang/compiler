8039725945364bb8a7ad858fc0652065506af855
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2025-03-29 22:23:25 -0500

    Add some test coverage

--- tests/15. exhaustive match.test.ts
+++ tests/15. exhaustive match.test.ts
@@ -26,6 +26,16 @@ let v = match(Some("hi!")) {
     expect(result.errors).toHaveLength(1);
 });
 
+test('Cases in result type', () => {
+  const code = `
+let v = match(Ok("hi!")) {
+  Ok(str) => "val: " + str
+  Err(int) => "err"
+}`;
+  const result = compile(code);
+  // all possibilities are covered so it should have no errors
+  expect(result.errors).toHaveLength(0);
+});
 
 test('Cases in match expr (covered by wildcard)', () => {
     const code = `
@@ -100,4 +110,43 @@ const welcomeText = match(u) {
     const result = compile(code);
     // All cases covered
     expect(result.errors).toHaveLength(0);
+});
+
+
+test('Cases in match expr against result type with Ok(T), Err(U) and T != U', () => {
+  const code = `match (Err(1)) {
+  Ok(2) => { "int" }
+  Ok(_) => "any other value"
+  Err(i) => {
+    const y = i + 1
+    "e"
+  }
+}`;
+  const result = compile(code);
+  // We expect an error because Ok(number) and Ok(string) shouldn't be unifiable
+  expect(result.errors).toHaveLength(0);
+});
+
+test('Cases in match expr against result that imply inconsistent T in Ok(T)', () => {
+  const code = `match (Err(1)) {
+  Ok(2) => "int"
+  Ok("2") => "str"
+  Ok(_) => "any other value"
+  Err(_) => "e"
+}`;
+  const result = compile(code);
+  // We expect an error because Ok(number) and Ok(string) shouldn't be unifiable
+  expect(result.errors).toHaveLength(1);
+});
+
+test('Cases in match expr against result that imply inconsistent U in Err(U)', () => {
+  const code = `match (Err(1)) {
+  Ok(_) => "any other value"
+  Err(1) => "e"
+  Err("2") => "e"
+  Err(_) => "e"
+}`;
+  const result = compile(code);
+  // We expect an error because Ok(number) and Ok(string) shouldn't be unifiable
+  expect(result.errors).toHaveLength(1);
 });
